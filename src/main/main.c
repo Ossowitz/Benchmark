21ff2e00d20336e3163f8a4e429103e6a9c56a4a
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-03-20 18:12:27 +0300

    Add quickSort

--- src/main/main.c
+++ src/main/main.c
@@ -127,7 +127,22 @@ struct Node* partition(struct Node* first, struct Node* last)
     return pivot;
 }
 
+// Performing quick sort in  the given linked list
+void quick_sort(struct Node* first, struct Node* last)
+{
+    if (first == last) {
+        return;
+    }
+    struct Node* pivot = partition(first, last);
 
+    if (pivot != NULL && pivot->next != NULL) {
+        quick_sort(pivot->next, last);
+    }
+
+    if (pivot != NULL && first != pivot) {
+        quick_sort(first, pivot);
+    }
+}
 
 // Функция для вывода массива
 void printArray(int *arr, size_t size) {
